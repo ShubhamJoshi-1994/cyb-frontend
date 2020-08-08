5c576a50c22a0a754a8f92ff2cd6b9634493454f
Shubham Joshi <shubham@betalectic.com>
2020-08-09 02:55:26 +0530

    integrated react-charts-2 library

--- src/components/chartSection.js
+++ src/components/chartSection.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import {Bar} from 'react-chartjs-2';
 
 function ChartSection() {
+    const state = {
+        labels: ['Cost as of Today', 'Cost in 10 Years'],
+        datasets: [
+            {
+                label: 'Rainfall',
+                backgroundColor: 'rgba(75,192,192,1)',
+                borderColor: 'rgba(0,0,0,1)',
+                borderWidth: 2,
+                data: [5000, 7000]
+            }
+        ]
+    }
+
     return (
-        <div>Chart</div>
+        <Bar
+            data={state}
+            height={100}
+            options={{
+                title:{
+                    display:true,
+                    text:'Average Rainfall per month',
+                    fontSize:16
+                },
+                legend:{
+                    display:true,
+                    position:'right'
+                },
+                maintainAspectRatio: false ,
+                scales: {
+                    xAxes: [{
+                        barPercentage: 0.4
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }}
+        />
     )
 }
 
